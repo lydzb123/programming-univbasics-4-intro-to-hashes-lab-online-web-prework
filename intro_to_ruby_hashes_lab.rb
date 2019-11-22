e00b7a0d1f7aa9e203fa6eeeecafc99ade0f6d3f
Lydia B <lydiabaik@gmail.com>
2019-11-22 06:54:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,6 +32,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
+  hash = {
+    key => correct_value
+  }
   # return the correct value using the hash and key parameters
 end
 
