58520d4acc8d64f966354e317dedb8276cea5757
Lydia B <lydiabaik@gmail.com>
2019-11-22 06:52:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ end
 
 def my_hash_creator(key, value)
   my_hash_creator = {
-    [key] => value
+    key => value
   }
   # return a hash that includes the key and value parameters passed into this method
 end
