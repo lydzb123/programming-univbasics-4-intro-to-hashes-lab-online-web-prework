f7b32938b2bb4040fdcbcc552ec40613d26a4359
Lydia B <lydiabaik@gmail.com>
2019-11-22 06:44:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,7 @@ def my_hash
   my_hash = {
     :noise_level => "too high"
   }
-  my_hash[]
-# return a valid hash with any key/value pair of your choice
+  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
