65c5abdf2c28e3c7dd3c91af213d7ac256870c39
Lydia B <lydiabaik@gmail.com>
2019-11-22 07:20:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-if hash[:key]
-  hash[:key] += 1
+if hash[key]
+  hash[key] += 1
 else
-  hash[:key] = 1
+  hash[key] = 1
 end
 end
     # given a hash an a key as parameters, return an updated hash
