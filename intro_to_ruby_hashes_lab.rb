120211fbf1eaee2764e3d5f49fafd117f8d7a580
Lydia B <lydiabaik@gmail.com>
2019-11-22 07:12:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,6 @@ if hash[key]
 else
   hash[key] + 1
 end
-update_counting_hash
 end
     # given a hash an a key as parameters, return an updated hash
     # if the provided key is not present in the hash, add it and assign it to the value of 1
